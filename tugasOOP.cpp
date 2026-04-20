c7f8b101f6e09ad5aebe09114ab697a47ea43ad9
sykllh <sykllhidmi@gmail.com>
2026-04-20 14:33:40 +0700

    membuat class barang

--- tugasOOP.cpp
+++ tugasOOP.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+class Barang{
+public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout  << "NIM : " << nim << endl;
+        cout  << "Nama : " << nama << endl;
+        cout  << "Nilai : " << nilai << endl;
+    }
+}; //batas class
+
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "Abra";
+    mhs.nilai = 90,5;
+
+    mhs.printData();
+}
